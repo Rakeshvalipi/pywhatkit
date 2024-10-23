5427afd9663233d57a2b1ef33f158ef77c688dc4
Rakeshvalipi <rakeshvalipi37@gmail.com>
2024-10-23 20:10:35 +0530

    Add files via upload

---
+++ pywhat mess.py
@@ -0,0 +1,8 @@
+import pywhatkit
+import time
+
+current_hour = time.localtime().tm_hour
+current_minute = time.localtime().tm_min
+
+
+pywhatkit.sendwhatmsg("+919059609017","hii yela vunaru",11,21 )
